0ed92d2aa9dec8724b65b7dc47842baf7b3d94d8
Mai Aggour <mayaaggourofficial@gmail.com>
2023-06-21 18:10:48 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main - entry point
+ *
+ * Describtion: prints putchar using _putchar prototype
+ *
+ * Return: always 0 (success)
+ *
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
+}
